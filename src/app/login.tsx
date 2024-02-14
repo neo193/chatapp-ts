94ad804e86572b2511e0e15a4152c57dee513859
DheerajShetty22 <dheerajshetty12345@gmail.com>
2024-02-14 09:08:19 +0530

    test

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -12,13 +12,12 @@ const Login = () => {
         <CardContent>
           <div className="flex flex-col space-y-2">
             <input type="text" placeholder="Username" className="border border-gray-400 rounded px-4 py-2" />
-            {/* <input type="email" placeholder="Email" className="border border-gray-400 rounded px-4 py-2" /> */}
             <input type="password" placeholder="Password" className="border border-gray-400 rounded px-4 py-2" />
             <button className="bg-blue-500 text-white px-4 py-2 rounded">Submit</button>
           </div>
         </CardContent>
         <CardFooter>
-          <Link href="/signup">SignUp</Link>
+          <Link href="/signup">Don't have a account? Sign Up</Link>
         </CardFooter>
       </Card>
     </div>
