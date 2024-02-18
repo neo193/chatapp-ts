047d4f37bf9645a626966fb14e15b62f79b04b51
DheerajShetty22 <dheerajshetty12345@gmail.com>
2024-02-18 17:20:40 +0530

    landing-ui

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import {auth,provider} from '@/app/utils/config'
 import {signInWithPopup} from 'firebase/auth'
+import { BackgroundGradientAnimation } from "../../components/ui/background-gradient-animation";
 import { useState } from 'react';
 function signup() {
   const [username, setUsername] = useState("");
@@ -34,16 +35,23 @@ function signup() {
 console.log(userData)
   }
   return (
-    <div className="flex justify-center items-center h-screen">
-    <Card className="border">
+    <div className="flex justify-center items-center h-screen bg-black">
+       <BackgroundGradientAnimation className=' h-screen w-[70%]'>
+      <div className="absolute z-50 inset-0 bg-gradient-to-r from-transparent to-black flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+        Ready to connect? Sign up.
+        </p>
+      </div>
+    </BackgroundGradientAnimation>
+    <Card className="border-none bg-black text-white">
       <CardHeader>
         <CardTitle>SignUp</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="flex flex-col space-y-2">
-          <input type="text"  onChange={(e) => setUsername(e.target.value)} placeholder="Username" className="border border-gray-400 rounded px-4 py-2" />
-          <input type="email"    onChange={(e) => setEmail(e.target.value)}placeholder="Email" className="border border-gray-400 rounded px-4 py-2" />
-          <input type="password"    onChange={(e) => setPassword(e.target.value)} placeholder="Password" className="border border-gray-400 rounded px-4 py-2" />
+        <div className="flex flex-col space-y-2 ">
+          <input type="text"  onChange={(e) => setUsername(e.target.value)} placeholder="Username" className=" bg-[#324DA5] rounded px-4 py-2" />
+          <input type="email"    onChange={(e) => setEmail(e.target.value)}placeholder="Email" className=" bg-[#324DA5] rounded px-4 py-2" />
+          <input type="password"    onChange={(e) => setPassword(e.target.value)} placeholder="Password" className=" bg-[#324DA5] rounded px-4 py-2" />
           <button onClick={handleRegister}className="bg-blue-500 text-white px-4 py-2 rounded border hover:border-black">Submit</button>
             <hr></hr>
             <Button onClick={handleGoogleRegisterClick}className='bg-white text-black font-bold border hover:border-black'>
